7f448263d1baa585b55dfae0a76b46a53ffa17cf
tuanphat <chongtuanphat2004@gmail.com>
2026-02-07 15:24:27 +0700

    Update allowed_hosts at settings.py

--- backend/phone/phone/settings.py
+++ backend/phone/phone/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # =========================
